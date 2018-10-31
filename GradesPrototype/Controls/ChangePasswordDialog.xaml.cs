487ebeb1400fff3406e6e1d411776a9f1c823997
jywkim <jkim589@gmail.com>
2018-10-31 13:55:40 -0400

    change password for existing user

--- GradesPrototype/Controls/ChangePasswordDialog.xaml.cs
+++ GradesPrototype/Controls/ChangePasswordDialog.xaml.cs
@@ -30,13 +30,38 @@ namespace GradesPrototype.Controls
         private void ok_Click(object sender, RoutedEventArgs e)
         {
             // TODO: Exercise 2: Task 4a: Get the details of the current user
-
+            User currentUser;
+            if (SessionContext.UserRole == Role.Teacher)
+            {
+                currentUser = SessionContext.CurrentTeacher;
+            }
+            else
+            {
+                currentUser = SessionContext.CurrentStudent;
+            }
             // TODO: Exercise 2: Task 4b: Check that the old password is correct for the current user
-
+            string oldPwd = oldPassword.Password;
+            if (!currentUser.VerifyPassword(oldPwd))
+            {
+                MessageBox.Show("Old password is incorrect", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
             // TODO: Exercise 2: Task 4c: Check that the new password and confirm password fields are the same
+            string newPwd = newPassword.Password;
+            string confirmPwd = confirm.Password;
 
+            if (String.Compare(newPwd, confirmPwd) != 0)
+            {
+                MessageBox.Show("The new password and confirm password fields are different", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
             // TODO: Exercise 2: Task 4d: Attempt to change the password
             // If the password is not sufficiently complex, display an error message
+            if (!currentUser.SetPassword(newPwd))
+            {
+                MessageBox.Show("The new password is not sufficiently complex", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
             // Indicate that the data is valid
             this.DialogResult = true;
