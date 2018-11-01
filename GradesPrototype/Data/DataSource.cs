86a1f51b65858ac743d12cbfacba94acc52b25f7
jywkim <jkim589@gmail.com>
2018-11-01 11:36:18 -0400

    start module 7 exercise 1 lab

--- GradesPrototype/Data/DataSource.cs
+++ GradesPrototype/Data/DataSource.cs
@@ -26,28 +26,29 @@ namespace GradesPrototype.Data
 
             Teachers = new List<Teacher>()
             {
-                new Teacher() { TeacherID = 1, UserName = "vallee", Password = "password99", FirstName = "Esther", LastName = "Valle", Class = "3C" },
-                new Teacher() { TeacherID = 2, UserName = "waited", Password = "password99", FirstName = "David", LastName = "Waite", Class = "4B" },
-                new Teacher() { TeacherID = 3, UserName = "newmanb", Password = "password99", FirstName = "Belinda", LastName = "Newman", Class = "2A" }
+                new Teacher() { TeacherID = 1, UserName = "valdezr", Password = "password99", FirstName = "Rachel", LastName = "Valdez", Class = "3C" },
+                new Teacher() { TeacherID = 2, UserName = "warrenj", Password = "password99", FirstName = "Jason", LastName = "Warren", Class = "4B" },
+                new Teacher() { TeacherID = 3, UserName = "nicolelloe", Password = "password99", FirstName = "Elena", LastName = "Nicolello", Class = "2A" }
             };
 
             Students = new List<Student>()
             {
                 new Student() { StudentID = 1, UserName = "liuk", Password = "password", TeacherID = 1, FirstName = "Kevin", LastName = "Liu" },
                 new Student() { StudentID = 2, UserName = "weberm", Password = "password", TeacherID = 1, FirstName = "Martin", LastName = "Weber" },
-                new Student() { StudentID = 3, UserName = "ligeorge", Password = "password", TeacherID = 1, FirstName = "George", LastName = "Li" },
-                new Student() { StudentID = 4, UserName = "millerl", Password = "password", TeacherID = 1, FirstName = "Lisa", LastName = "Miller" },
+                new Student() { StudentID = 3, UserName = "leeo", Password = "password", TeacherID = 1, FirstName = "Oliver", LastName = "Lee" },
+                new Student() { StudentID = 4, UserName = "mulvado", Password = "password", TeacherID = 1, FirstName = "Olga", LastName = "Mulvad" },
                 new Student() { StudentID = 5, UserName = "liur", Password = "password", TeacherID = 1, FirstName = "Run", LastName = "Liu" },
-                new Student() { StudentID = 6, UserName = "stewarts", Password = "password", TeacherID = 2, FirstName = "Sean", LastName = "Stewart" },
+                new Student() { StudentID = 6, UserName = "sousaf", Password = "password", TeacherID = 2, FirstName = "Fernando", LastName = "Sousa" },
                 new Student() { StudentID = 7, UserName = "turnero", Password = "password", TeacherID = 2, FirstName = "Olinda", LastName = "Turner" },
                 new Student() { StudentID = 8, UserName = "ortonj", Password = "password", TeacherID = 2, FirstName = "Jon", LastName = "Orton" },
-                new Student() { StudentID = 9, UserName = "nixont", Password = "password", TeacherID = 2, FirstName = "Toby", LastName = "Nixon" },
-                new Student() { StudentID = 10, UserName = "guinotd", Password = "password", TeacherID = 2, FirstName = "Daniela", LastName = "Guinot" },
+                new Student() { StudentID = 9, UserName = "thakura", Password = "password", TeacherID = 2, FirstName = "Abhijit", LastName = "Thakur" },
+                new Student() { StudentID = 10, UserName = "liveiras", Password = "password", TeacherID = 2, FirstName = "Susana", LastName = "Oliveira" },
                 new Student() { StudentID = 11, UserName = "sundaramv", Password = "password", TeacherID = 3, FirstName = "Vijay", LastName = "Sundaram" },
                 new Student() { StudentID = 12, UserName = "grubere", Password = "password", TeacherID = 3, FirstName = "Eric", LastName = "Gruber" },
-                new Student() { StudentID = 13, UserName = "meyerc", Password = "password", TeacherID = 3, FirstName = "Chris", LastName = "Meyer" },
-                new Student() { StudentID = 14, UserName = "liyuhong", Password = "password", TeacherID = 3, FirstName = "Yuhong", LastName = "Li" },
-                new Student() { StudentID = 15, UserName = "liyan", Password = "password", TeacherID = 3, FirstName = "Yan", LastName = "Li" }        
+                new Student() { StudentID = 13, UserName = "lyonw", Password = "password", TeacherID = 3, FirstName = "William", LastName = "Lyon" },
+                new Student() { StudentID = 14, UserName = "leea", Password = "password", TeacherID = 3, FirstName = "Aaron", LastName = "Lee" },
+                new Student() { StudentID = 15, UserName = "leec", Password = "password", TeacherID = 3, FirstName = "Christina", LastName = "Lee" }
+        
             };
 
             Grades = new List<Grade>()
