b61d1840d50a6537c466b3c8dcbc0036fd69e789
jywkim <jkim589@gmail.com>
2018-11-01 18:53:40 -0400

    specify GradesDBEntities data context for service

--- Grades.Web/Services/GradesWebDataService.svc.cs
+++ Grades.Web/Services/GradesWebDataService.svc.cs
@@ -7,15 +7,22 @@ using System.Text;
 using System.ServiceModel.Web;
 using System.Data.Services.Common;
 using System.Data.Services;
+using Grades.DataModel;
 
 namespace Grades.Web.Services
 {
     //TODO: Excersice 1: Task 2a: Replace the object keyword with your data source class name.
-    public class GradesWebDataService : DataService<object>
+    public class GradesWebDataService : DataService<SchoolGradesDBEntities>
     {
         public static void InitializeService(DataServiceConfiguration config)
         {
             //TODO: Excersice 1: Task 2b: set rules to indicate which entity sets and service operations are visible, updatable, etc
+            // Configure all entity sets to permit read and write access.
+            config.SetEntitySetAccessRule("Grades", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Teachers", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Students", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Subjects", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Users", EntitySetRights.All);
 
         }
 
